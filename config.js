d5b82f91aa23d339035f5f4a110f65e7e427c487
Medhouasly <152456900+Medhouasly@users.noreply.github.com>
2024-02-02 12:30:26 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  token: "MTIwMjkzNDAwNDM0MTUzODg4Ng.GqaAAy.ZDbsR361EToDZJcY_23O3VSznPi-cfUX5hyCQE",
+  parentId: '1190342071303815311',
+  probot_ids: ['282859044593598464'], //ايدي برو بوت
+  recipientId: '1117509477957116044', //ايدي الي حتحولو
+  price: 2 //السعر ب الضريبة
+} 
